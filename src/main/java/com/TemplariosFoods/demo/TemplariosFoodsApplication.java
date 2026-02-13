daecb8ed9e5c9f3cfe64f352cfad55d0ae6749c5
ROKURO456 <mateoch9@hotmail.com>
2026-02-13 13:11:41 -0500

    commit 1 admin

---
+++ src/main/java/com/TemplariosFoods/demo/TemplariosFoodsApplication.java
@@ -0,0 +1,13 @@
+package com.TemplariosFoods.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplariosFoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplariosFoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TemplariosFoods/demo/TemplariosFoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TemplariosFoods.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplariosFoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
